19d34404d3d220aaa9d61e5d87f6b9576054c683
shravankaja <60179805+shravankaja@users.noreply.github.com>
2021-03-18 21:11:53 +0530

    Added: method to read all the rows from table in database

--- src/main/java/com/employeepayrolljdbc/EmployeePayRollService.java
+++ src/main/java/com/employeepayrolljdbc/EmployeePayRollService.java
@@ -1,12 +1,51 @@
 package com.employeepayrolljdbc;
 
 import java.sql.*;
+import java.time.*;
 import java.util.*;
 
 public class EmployeePayRollService {
+
+    String name;
+    LocalDate startDate;
+    int salary;
+    int id;
+
+    public EmployeePayRollService() {
+
+    }
+
+    public EmployeePayRollService(String name, LocalDate startDate, int salary, int id) {
+        this.name = name;
+        this.startDate = startDate;
+        this.salary = salary;
+        this.id = id;
+    }
+
+    public ArrayList<EmployeePayRollService> readData(String url, String userName, String password) {
+        ArrayList<EmployeePayRollService> listOfDataObjects = new ArrayList<>();
+        String sql = "select * from employee_payroll";
+        try {
+            Connection connection = this.connectToDatabase(url, userName, password);
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(sql);
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                int salary = resultSet.getInt("salary");
+                LocalDate startdate = resultSet.getDate("start").toLocalDate();
+                listOfDataObjects.add(new EmployeePayRollService(name, startdate, salary, id));
+            }
+            connection.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return listOfDataObjects;
+    }
+
     // Connection connection;  //database connection
-    public String connectToDatabase(String url, String userName, String password) {
-        Connection connection;  //database connection
+    public Connection connectToDatabase(String url, String userName, String password) {
+        Connection connection = null;  //database connection
         String connectionString = null;
         try {
             Class.forName("com.mysql.jdbc.Driver");
@@ -23,7 +62,7 @@ public class EmployeePayRollService {
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
-        return connectionString;
+        return connection;
     }
 
     public ArrayList<String> listDrivers() {
@@ -41,7 +80,7 @@ public class EmployeePayRollService {
         System.out.println("Welcome to payroll service");
         // localhost is the server name where database exsists or it can be IP address
         // 3306 is the port number , payroll_services is the database name
-        String jdbcURL = "jdbc:mysql://localhost:3306/payroll_services?allowPublicKeyRetrieval=true&useSSL=false";
+        String jdbcURL = "jdbc:mysql://localhost:3306/employee_payroll_service?allowPublicKeyRetrieval=true&useSSL=false";
         String userName = "root";
         String password = "Addtexthere25";
         EmployeePayRollService employeePayRollService = new EmployeePayRollService();
