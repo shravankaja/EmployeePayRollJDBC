fcd799c9e366d21d68c2769b2c0198349d07b1b3
shravankaja <60179805+shravankaja@users.noreply.github.com>
2021-03-18 20:21:14 +0530

    Added: initlialized class and added welcome message

---
+++ src/main/java/com/employeepayrolljdbc/EmployeePayRollService.java
@@ -0,0 +1,7 @@
+package com.employeepayrolljdbc;
+
+public class EmployeePayRollService {
+    public static void main(String Agrs[]) {
+        System.out.println("Welcome to Employee payroll service");
+    }
+}
