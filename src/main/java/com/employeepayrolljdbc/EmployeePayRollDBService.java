ed78c80f4448981a19fd6fc0454681138b4b62a7
shravankaja <60179805+shravankaja@users.noreply.github.com>
2021-03-19 08:56:23 +0530

    Added: created a singleton object and prepared statment to retrieve records for specific name

---
+++ src/main/java/com/employeepayrolljdbc/EmployeePayRollDBService.java
@@ -0,0 +1,127 @@
+package com.employeepayrolljdbc;
+
+import java.sql.*;
+import java.time.*;
+import java.util.*;
+
+public class EmployeePayRollDBService {
+
+    //Defining Singleton object by making a private constructor
+    private static EmployeePayRollDBService employeePayRollDBService;
+    private PreparedStatement getEmployeePayRollData;
+
+    private EmployeePayRollDBService() {
+
+    }
+
+    // method to create a new singleton object
+    public static EmployeePayRollDBService getInstance() {
+        if (employeePayRollDBService == null) {
+            employeePayRollDBService = new EmployeePayRollDBService();
+        }
+        return employeePayRollDBService;
+    }
+
+    private void preparedStatmentForWholeTableData() {
+        try {
+            Connection connection = this.connectToDatabase();
+            String sql = "select * from employee_payroll where name = ?";
+            getEmployeePayRollData = connection.prepareStatement(sql);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    public ArrayList<EmployeePayRollService> getDataFromEmployeePayRollTable(String name) {
+        ArrayList<EmployeePayRollService> list = new ArrayList<>();
+        if (getEmployeePayRollData == null) this.preparedStatmentForWholeTableData();
+        try {
+            getEmployeePayRollData.setString(1, name);
+            ResultSet resultSet = getEmployeePayRollData.executeQuery();
+            list = this.getListFromResultSet(resultSet);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return list;
+    }
+
+    private ArrayList<EmployeePayRollService> getListFromResultSet(ResultSet resultSet) {
+        ArrayList<EmployeePayRollService> list = new ArrayList<>();
+        try {
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                int salary = resultSet.getInt("salary");
+                LocalDate startdate = resultSet.getDate("start").toLocalDate();
+                list.add(new EmployeePayRollService(name, startdate, salary, id));
+            }
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return list;
+    }
+
+    public ArrayList<EmployeePayRollService> readData() {
+        ArrayList<EmployeePayRollService> listOfDataObjects = new ArrayList<>();
+        String sql = "select * from employee_payroll";
+        try {
+            Connection connection = this.connectToDatabase();
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(sql);
+            listOfDataObjects = this.getListFromResultSet(resultSet);
+            connection.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return listOfDataObjects;
+    }
+
+    // Connection connection;  //database connection
+    public Connection connectToDatabase() {
+        String jdbcURL = "jdbc:mysql://localhost:3306/employee_payroll_service?allowPublicKeyRetrieval=true&useSSL=false";
+        String userName = "root";
+        String password = "Addtexthere25";
+        Connection connection = null;  //database connection
+        String connectionString = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("Driver loaded");
+
+        } catch (ClassNotFoundException e) {
+            throw new IllegalStateException("No drivers loaded ", e);
+        }
+        try {
+            System.out.println("Connectin to " + jdbcURL);
+            connection = DriverManager.getConnection(jdbcURL, userName, password);
+            System.out.println(connection);
+            connectionString = connection.toString();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return connection;
+    }
+
+    public ArrayList<String> listDrivers() {
+        Enumeration<Driver> driverList = DriverManager.getDrivers();
+        ArrayList<String> listOfDrivers = new ArrayList<>();
+        while (driverList.hasMoreElements()) {
+            Driver driver = (Driver) driverList.nextElement();
+            System.out.println("  " + driver.getClass().getName());
+            listOfDrivers.add(driver.getClass().getName());
+        }
+        return listOfDrivers;
+    }
+
+    public int updateSalary(String name, int salary) {
+        String sql = String.format("update  employee_payroll set salary=%d where name='%s'", salary, name);
+        try {
+            Connection connection = this.connectToDatabase();
+            Statement statement = connection.createStatement();
+            return statement.executeUpdate(sql);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return 0;
+    }
+}
